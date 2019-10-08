a3ad08c04098b609ea074dc9c9a30d8c524dc422
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-08 15:22:38 +0200

    first steps

--- basic03-map-counter/xdp_avb_kern.c
+++ basic03-map-counter/xdp_avb_kern.c
@@ -113,8 +113,6 @@ int  xdp_avtp_func(struct xdp_md *ctx)
 
 	struct hdr_cursor nh;
 	int nh_type;
-    struct iphdr *ipheader;
-	unsigned char ip_proto_type;
 
 	/* Start next header cursor position at data start */
 	nh.pos = data;
@@ -126,7 +124,8 @@ int  xdp_avtp_func(struct xdp_md *ctx)
 	nh_type = parse_ethhdr(&nh, data_end, &eth);
 
     if( nh_type == bpf_htons(ETH_P_IP) ){ //ETH_P_TSN = 0x22f0
-        ip_proto_type = parse_iphdr(&nh, data_end, &ipheader);
+        struct iphdr *ipheader;
+        unsigned char ip_proto_type = parse_iphdr(&nh, data_end, &ipheader);
         lock_xadd(&rec->rx_packets, 1);
 
         if( ip_proto_type == IPPROTO_ICMP ){
