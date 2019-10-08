197af50a3b2447307c0ca431feff867c19ec4bb5
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-08 13:40:29 +0200

    first steps

---
+++ AVB_XDP/common_kern_user.h
@@ -0,0 +1,18 @@
+/* This common_kern_user.h is used by kernel side BPF-progs and
+ * userspace programs, for sharing common struct's and DEFINEs.
+ */
+#ifndef __COMMON_KERN_USER_H
+#define __COMMON_KERN_USER_H
+
+/* This is the data record stored in the map */
+struct datarec {
+	__u64 rx_packets;
+	__u64 accu_rx_timestamp;
+	/* Assignment#1: Add byte counters */
+};
+
+#ifndef XDP_ACTION_MAX
+#define XDP_ACTION_MAX (XDP_REDIRECT + 1)
+#endif
+
+#endif /* __COMMON_KERN_USER_H */
