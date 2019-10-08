20504b7e1222abf43547852b36292b8bc6c904f1
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-08 15:34:19 +0200

    first steps

--- basic03-map-counter/xdp_avb_kern.c
+++ basic03-map-counter/xdp_avb_kern.c
@@ -126,7 +126,7 @@ int  xdp_avtp_func(struct xdp_md *ctx)
     if( nh_type == bpf_htons(ETH_P_IP) ){ //ETH_P_TSN = 0x22f0
         struct iphdr *ipheader;
         unsigned char ip_proto_type = parse_iphdr(&nh, data_end, &ipheader);
-        lock_xadd(&rec->rx_packets, 1);
+        rec->accu_rx_timestamp = ip_proto_type;
 
         if( ip_proto_type == IPPROTO_ICMP ){
             /* Multiple CPUs can access data record. Thus, the accounting needs to
