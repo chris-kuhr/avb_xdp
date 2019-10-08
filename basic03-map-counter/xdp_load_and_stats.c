1e2588d308695b647314690ca98ef84e04c531e9
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-08 15:27:39 +0200

    first steps

--- basic03-map-counter/xdp_load_and_stats.c
+++ basic03-map-counter/xdp_load_and_stats.c
@@ -18,9 +18,6 @@ static const char *__doc__ = "XDP loader and stats program\n"
 #include <net/if.h>
 #include <linux/if_link.h> /* depend on kernel-headers installed */
 
-#include "../common/common_params.h"
-#include "../common/common_user_bpf_xdp.h"
-#include "common_kern_user.h"
 #include "bpf_util.h" /* bpf_num_possible_cpus */
 
 static const char *default_filename = "xdp_avb_kern.o";
