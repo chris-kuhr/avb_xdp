77746316ff8a5af540a013264a94c26602838a6d
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-10 18:34:49 +0200

    depayloading

--- basic03-map-counter/xdp_load_and_stats.c
+++ basic03-map-counter/xdp_load_and_stats.c
@@ -274,6 +274,8 @@ int main(int argc, char **argv)
 	/* Cmdline options can change progsec */
 	parse_cmdline_args(argc, argv, long_options, &cfg, __doc__);
 
+	printf( ((1U << ((sizeof(int)*8)-1))-1) );
+
 	/* Required option */
 	if (cfg.ifindex == -1) {
 		fprintf(stderr, "ERR: required option --dev missing\n");
