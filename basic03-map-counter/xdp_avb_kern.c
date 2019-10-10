6a949fa59fbafcea55a57c7296d13f666b2edc86
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-10-10 18:37:27 +0200

    depayloading

--- basic03-map-counter/xdp_avb_kern.c
+++ basic03-map-counter/xdp_avb_kern.c
@@ -149,7 +149,7 @@ int  xdp_avtp_func(struct xdp_md *ctx)
                         __u32 sample = bpf_htonl(avtpSamples[i+j]);
                         sample &= 0x00ffffff;
                         sample <<= 8;
-                        rec->sampleBuffer[i][j] = ((int)sample)/(float)(2);/* use tail here */
+                        //rec->sampleBuffer[i][j] = ((int)sample)/(float)(2);/* use tail here */
                         lock_xadd(&rec->sampleCounter, 1);
                     }
                 }
