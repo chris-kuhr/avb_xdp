d746c3c8ae8124a620b1d9ba4739f44e09a0f946
Christoph Kuhr <christoph.kuhr@hs-anhalt.de>
2019-11-05 12:16:36 +0100

    access mem_map - handle return 0xff and 0xffff code from parsers

--- basic03-map-counter/xdp_avb_kern.c
+++ basic03-map-counter/xdp_avb_kern.c
@@ -142,22 +142,23 @@ int  xdp_avtp_func(struct xdp_md *ctx)
                 __u8 audioChannels = parse_61883hdr(&nh, data_end, &hdr61883);
                 if( 0xff == audioChannels )
                     return XDP_PASS;
-                __u32 *avtpSamples = (__u32*)nh.pos;
 
-
-
-                int i,j;
-                #pragma unroll
-                for(j=0; j<AUDIO_CHANNELS;j++){
-
-                    #pragma unroll
-                    for(i=0; i<6*AUDIO_CHANNELS;i+=AUDIO_CHANNELS){
-                        __u32 sample = bpf_htonl(avtpSamples[i+j]) & 0x00ffffff;
-                        sample <<= 8;
-                        rec->sampleBuffer[j][i] = (int) sample;//(float)((int)sample);///(float)(2);// use tail here
-                        rec->sampleCounter++;
-                    }
-                }
+//                __u32 *avtpSamples = (__u32*)nh.pos;
+//
+//
+//
+//                int i,j;
+//                #pragma unroll
+//                for(j=0; j<AUDIO_CHANNELS;j++){
+//
+//                    #pragma unroll
+//                    for(i=0; i<6*AUDIO_CHANNELS;i+=AUDIO_CHANNELS){
+//                        __u32 sample = bpf_htonl(avtpSamples[i+j]) & 0x00ffffff;
+//                        sample <<= 8;
+//                        rec->sampleBuffer[j][i] = (int) sample;//(float)((int)sample);///(float)(2);// use tail here
+//                        rec->sampleCounter++;
+//                    }
+//                }
 
 
 
